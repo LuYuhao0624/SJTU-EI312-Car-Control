904a5866f3f410114b42192f29589dfc04ea1e27
LuYuhao666 <dtassassin@sjtu.edu.cn>
2019-12-12 11:43:32 +0800

    Move repo root to the parent folder to include CarClient

---
+++ CarController/app/src/main/java/com/car/AbsoluteFragment.java
@@ -0,0 +1,29 @@
+package com.car;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AbsoluteFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AbsoluteFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AbsoluteFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_absolute, container, false);
+        return root;
+    }
+}
